e7557de842d09b757a5265e75f9f09fd3d9c9384
AnnPython <74019824+AnnPython@users.noreply.github.com>
2021-01-16 23:02:01 +0200

    Add files via upload

--- plot_norm.py
+++ plot_norm.py
@@ -2,139 +2,64 @@ import vidget_norm
 import rozr_ok
 from rozr_ok import *
 import pandas as pd
-import xlsxwriter
+#import xlsxwriter
 from openpyxl import Workbook
-from openpyxl.chart import (Reference,
-    Series,
-    BarChart3D,)
+from openpyxl.chart import (Reference,Series,BarChart3D,)
 from openpyxl import load_workbook
-from openpyxl.styles import NamedStyle
+#from openpyxl.styles import NamedStyle
 from openpyxl.styles import Font, Color, Alignment, Border, Side, colors
 from openpyxl import cell
-#from openpyxl.cell import get_column_letter, column_index_from_string
+
 
 file_name = str(period.get()) + '.xlsx'
 wb = load_workbook(file_name )
 sheet = wb.active
 ws = wb.active
 
-sheet = wb.get_sheet_by_name('Ан')
-tuple(sheet['A1':'D35'])
+sheet.column_dimensions["A"].width = 27
+sheet.column_dimensions["B"].width = 15
+sheet.column_dimensions["C"].width = 15
+sheet.column_dimensions["D"].width = 15
+sheet.column_dimensions["E"].width = 15
 
-for rowOfCellObjects in sheet['A1':'D35']:
-    max_length = 0
-    column = rowOfCellObjects[0].column
-    
-    for cellObj in rowOfCellObjects:
-        if len(str(cellObj.value)) > max_length:
-            max_length = len(cellObj.value)
-        
-            adjusted_width = (max_length + 2) * 1.2
-            ws.column_dimensions[column].width = adjusted_width
+thin = Side(border_style="thin", color="303030") 
+black_border = Border(top=thin, left=thin, right=thin, bottom=thin)
+font = Font(name='Times New Roman', size=10, bold=False, color='07101c')
+align = Alignment(horizontal="center", wrap_text= True, vertical="center")
+
+for label in ["A", "B", "C", "D", "E"]: 
+    for col_idx in range(34):
+        idx = label + str(col_idx + 1) 
+        sheet[idx].alignment = align 
+        sheet[idx].font = font 
+
+for row in ws['A2:E10']:
+    for cell in row:
+        cell.border = black_border
 
 
+for row in ws['A15:D19']:
+    for cell in row:
+        cell.border = black_border
+
+for row in ws['A23:C24']:
+    for cell in row:
+        cell.border = black_border
 
 
+for row in ws['A27:B34']:
+    for cell in row:
+        cell.border = black_border         
 '''
-for rowOfCellObjects in sheet['A1':'D35']:
-    for cellObj in rowOfCellObjects:
-       if cellObj is not None:
-           ws.column_dimensions['A'].width = 35
-       elif cellObj is not None:
-           ws.row_dimensions[1].width = 15
-       else:
-            pass
-               
-'''
+df.to_excel(writer, sheet_name="Sheet 1"
+workbook = load_workbook(file_name )
+sheet = workbook.active
+format1 = workbook.add_format({'num_format': '0.00'})
+sheet.set_column('E:E', 20, format1)
+workbook.save(file_name)
 
-'''
-for col in sheet.columns:
-    for j in  range(len(col)):
-        if j==0:
-            ws.column_dimensions.width = 10
-        elif j>=4:
-            ws.column_dimensions.width = 35
-            
-
-'''
-
-'''
-ws.column_dimensions['A'].width = 35
-ws.column_dimensions['B'].width = 20
-ws.column_dimensions['c'].width = 20
-ws.column_dimensions['D'].width = 20
-'''
-
-
-
-
-wb.save(file_name)
-'''
-ns = NamedStyle(name='highlight')
-ns.font = Font(bold=True, size=14)
-border = Side(style='thick', color='000000')
-ns.border = Border(left=border, top=border, right=border, bottom=border)
-
-
-wb.add_named_style(ns)
-
-for col in sheet.columns:
-        for cell in col:
-            sheet.style = 'highlight'
-
-
-
-'''
-
-
-
-
-
-
-'''work
-ws.column_dimensions['A'].width = 35
-ws.column_dimensions['B'].width = 25
-'''
-'''
-ws.column_dimensions['A'].width = 35
-ws.row_dimensions[1].width = 25
-'''
-'''
-# Create a few styles
-bold_font = Font(bold=True)
-
-center_aligned_text = Alignment(horizontal="center")
-double_border_side = Side(border_style="double")
-square_border = Border(top=double_border_side,
-                        right=double_border_side,
-                        bottom=double_border_side,
-                        left=double_border_side)
-
-
-for col in sheet.columns:
-        for cell in col:
-            #column_dimensions.width = 35
-            #row_dimensions.width = 25
-            alignment = center_aligned_text
-            border = square_border
-          
-            
-'''
-
-'''
- # Style some cells!
-sheet['A2'].font = bold_font
-
-sheet['A2'].alignment = center_aligned_text
-sheet['A2'].border = square_border
-
-'''
-
-
-
-
-
-
+'''        
+wb.save(file_name)      
 
 
 wb1 = load_workbook(file_name )
